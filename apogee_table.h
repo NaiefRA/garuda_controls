b16fc1a0ae3e3e4c04033a2c3b2ed3366ea1b1b8
NaiefRA <ashrafnaiefryan@gmail.com>
2026-06-19 04:36:01 +0530

    new lookup table + smc changes

--- apogee_table.h
+++ apogee_table.h
@@ -2,15 +2,15 @@
 #include <Arduino.h> // Required for the PROGMEM macro
 
 // Grid: 43 alts x 41 machs x 21 defls x 11 incls
-const int N_ALT  = 43;
+const int N_ALT  = 25;
 const int N_MACH = 41;
 const int N_DEF  = 21;
 const int N_INCL = 11;
 
-const float ALT_MIN  = 1000.0f, ALT_STEP  = 50.0f;
+const float ALT_MIN  = 1900.0f, ALT_STEP  = 50.0f;
 const float MACH_MIN = 0.00f,   MACH_STEP = 0.02f;
 const float DEF_MIN  = 0.0f,    DEF_STEP  = 3.0f;
 const float INCL_MIN = 0.0f,    INCL_STEP = 1.0f;
 
 // Add PROGMEM here
-extern const float PROGMEM APOGEE_DELTA[43][41][21][11];
+extern const float PROGMEM APOGEE_DELTA[25][41][21][11];
